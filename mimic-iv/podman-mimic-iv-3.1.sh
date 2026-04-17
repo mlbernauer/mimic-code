fe93b8299c8c600caa2fd685d218e33e660b933a
Michael Bernauer <mlbernauer@gmail.com>
2026-04-17 15:16:53 -0600

    update db path

--- mimic-iv/podman-mimic-iv-3.1.sh
+++ mimic-iv/podman-mimic-iv-3.1.sh
@@ -3,7 +3,7 @@ podman run -it -d \
   --name mimic-db-31 \
   -e POSTGRES_PASSWORD=$MIMIC_POSTGRES_PASSWORD \
   -e PGDATA=/mnt/pgdata/ \
-  -v $HOME/data/pg/mimiciv/3.1/pgdata/:/mnt/pgdata/ \
+  -v /mnt/spare/mimic-pg/mimiciv/3.1/pgdata/:/mnt/pgdata/ \
   -v .:/mnt/mimic-code/ \
   -p 5432:5432 \
   docker.io/postgres:15
